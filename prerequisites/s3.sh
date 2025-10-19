c10b0572acc847e72f8b1df5d69e4fac3f403249
Markus J. Nagel <mnagel@redhat.com>
2025-10-19 18:43:23 +0200

    changed s3 printout

--- prerequisites/s3.sh
+++ prerequisites/s3.sh
@@ -2,18 +2,18 @@
 
 CLAIM="tpa-bucket-claim"
 
-echo -n "Access Key ID: "
+echo -n "Access Key ID:       "
 oc get secret $CLAIM -o jsonpath='{.data.AWS_ACCESS_KEY_ID}' | base64 -d
 echo ""
 
-echo -n "Secret Access Key: "
+echo -n "Secret Access Key:   "
 oc get secret $CLAIM -o jsonpath='{.data.AWS_SECRET_ACCESS_KEY}' | base64 -d
 echo ""
 
-echo -n "Endpoint: "
+echo -n "Endpoint:            "
 oc get route s3 -n openshift-storage -o jsonpath='https://{.spec.host}'
 echo ""
 
-echo -n "Bucket Name: "
+echo -n "Bucket Name:         "
 oc get configmap $CLAIM -o jsonpath='{.data.BUCKET_NAME}'
 echo ""
