e931f20352d8c3e84fb58a39825f3aed766966d1
Markus J. Nagel <mnagel@redhat.com>
2025-10-27 15:34:45 +0100

    example for OIDC access tokens

---
+++ security-concepts/get-access-token.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get the cluster ingress domain
+INGRESS_DOMAIN=$(oc get ingresses.config/cluster -o jsonpath='{.spec.domain}')
+
+# Construct the ISSUER_URL
+ISSUER_URL="https://sso.${INGRESS_DOMAIN}/realms/trusted-artifact-signer"
+
+# Set the client ID
+CLIENT_ID="trusted-artifact-signer"
+
+# Set username and password
+USERNAME="pipeline-auth@demo.redhat.com"
+PASSWORD="r3dh8t1!"
+
+# Get the access token
+TOKEN=$(curl -s -X POST "${ISSUER_URL}/protocol/openid-connect/token" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "grant_type=password" \
+  -d "client_id=${CLIENT_ID}" \
+  -d "username=${USERNAME}" \
+  -d "password=${PASSWORD}" \
+  | jq -r '.access_token')
+
+echo "Access Token: ${TOKEN}"
